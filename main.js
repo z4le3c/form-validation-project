ea92674b92401f09e490efa825f4fe92feaba3c0
zalec <142818531+z4le3c@users.noreply.github>
2023-11-23 19:13:44 -0600

    add email inline validation

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
+const emailInput = document.querySelector('#email')
+const emailInvalidMessage = document.querySelector('#email-invalid-message')
+const countrySelect = document.querySelector('#country')
+const inputZipCode = document.querySelector('#zip-code')
 const sendButton = document.querySelector('#send-button')
 
-sendButton.addEventListener('click', (event) => {
-  event.preventDefault()
-  console.log('in')
+emailInput.addEventListener('focusout', () => {
+  if (emailInput.value == '') {
+    emailInvalidMessage.textContent = 'please provide an email'
+  } else if (emailInput.validity.typeMismatch) {
+    emailInvalidMessage.textContent = 'provide a valid email, with this format email@example'
+  }
+})
+
+emailInput.addEventListener('input', () => {
+  emailInvalidMessage.textContent = ''
 })
